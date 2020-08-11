6dfa2def23f0867f4553638f3c1f398fc945b271
todorr92 <todorovicslobodan10@gmail.com>
2020-08-11 13:50:56 +0000

    deleting multiple names using formated string

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,8 +1,7 @@
 import os
-import datetime
 import pymysql
 
-# Get username from Cloud9 workspace
+# Get the username from the Cloud9 workspace
 # (modify this variable if running on another environment)
 username = os.getenv('C9_USER')
 
@@ -11,13 +10,15 @@ connection = pymysql.connect(host='localhost',
                              user=username,
                              password='',
                              db='Chinook')
-
 try:
-    # Run a query
     with connection.cursor() as cursor:
-        rows = cursor.executemany("DELETE FROM Friends WHERE name = %s;", ['Bob', 'Jim'])
+        list_of_names = ['fred', 'Fred']
+        # Prepare a string with same number of placeholders as in list_of_names
+        format_strings = ','.join(['%s'] * len(list_of_names))
+        cursor.execute(
+            "DELETE FROM Friends WHERE name in ({});".format(format_strings),
+            list_of_names)
+
         connection.commit()
 finally:
-    # Close the connection, regardless of whether or not 
-    # the above was successful
     connection.close()
